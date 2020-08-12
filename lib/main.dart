dc32ada36a8c17b05c7bbb70a3816c159a5c82e9
israel <aminuisrael2@gmail.com>
2020-08-12 00:24:33 +0000

    added List View

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,36 @@ import './app_screens/home.dart';
 void main() {
   runApp(MaterialApp(
     title: "Exploring UI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Basic List View"),
+      ),
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: [
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("LandScape Typed");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Windows"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      )
+    ],
+  );
+  return listView;
 }
